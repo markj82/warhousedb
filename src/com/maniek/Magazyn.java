38f24a5d0b48e4660420e9fdd3c2320a85a9e375
marek <amigomarek@gmail.com>
2017-11-24 09:56:37 +0100

    first commit

---
+++ src/com/maniek/Magazyn.java
@@ -0,0 +1,4 @@
+package com.maniek;
+
+public class Magazyn {
+}
---
+++ src/com/maniek/Main.java
@@ -0,0 +1,18 @@
+package com.maniek;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //magazzyn, w ktorym mam produkty, ile mam danego produktu?
+// stworzyc klase produkt, nazwe i ilosc
+// magayn bedzie mial liste produktow
+// metoda wypisania wszystkich produktow, wypisz stan magazynu
+// suma wszystkich produktow
+// doaj nowy produkt
+// usun produkt
+// sprawdz ile masz danego produktu
+
+//test dodawania produktu do magazunu
+
+    }
+}
